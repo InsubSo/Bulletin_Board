24e399f48fe196955ddc34020e469bcce67762f9
IS So <soinsubs1@gmail.com>
2025-03-26 13:16:49 +0900

    2025_03_36_1300

--- pages/2_Search_ATAC_Model.py
+++ pages/2_Search_ATAC_Model.py
@@ -87,10 +87,11 @@ def sidebar_for_model_registration_form():
             "401 HVM": [
                 "23FLEXFSE-K", "23FLEXFS-K", "23FLEXFL-K", "23FLEXFL_AMMP-K","23FLEXFX-K",
                 "23FLEXGL-K", "23FLEXGB-K", "23FLEXGS",
-                "23FLEXHX","23FLEXHXE", "23FLEXHXE_CRYO", "23FLEXHXPL_CRYO", "23FLEXJXP"
+                "23FLEXHX", "23FLEXHX_PLUS","23FLEXHXE", "23FLEXHXE_CRYO", "23FLEXHXPL_CRYO",
+                "23FLEXJXP"
             ],
             "416 UPL": [
-                "FLEXGX/GXE-HXPL", "FLEXHXPL-HXPLC", "FLEXHXPLHXE_CM", "FLEXHX_PLUS-HXE"
+                "FLEXGX/GXE-HXPL", "FLEXHXPL-HXPLC", "FLEXHXPLHXE_CM", "FLEXHX_PLUS-HXE", "FLEXHXE-HXEC", "FLEXGX-GXE-HX_U", "FLEXHX--HX_PLUS"
             ]
         }
         st.selectbox("LPR Model Name", lpr_model_options[selected_pom], key='lpr_model_name')
@@ -108,8 +109,10 @@ def sidebar_for_model_registration_form():
                 "FlexHSeriesBiasElectrode", "FlexJXPBiasElectrode", "FlexHXPlusBiasElectrode"
             ],
             "PM": [
-                "FlexFSeries", "FlexFLSeries", "FlexGLSeries", "FlexGBPM",
-                "FlexGS", "FlexHXEPM", "FlexHSeries", "FlexJXP"
+                "FlexFSeries", "FlexFLSeries",
+                "FlexGLSeries", "FlexGBPM", "FlexGS",
+                "FlexHXEPM", "FlexHSeries", "FlexHXPlus",
+                "FlexJXP"
             ]
         }
         selected_model_number = st.selectbox("ATAC Model Number", atac_model_options[selected_type_class], key='model_number')
@@ -120,16 +123,16 @@ def sidebar_for_model_registration_form():
             "FlexGSRFCart": ["FlexGSRFCart_UI-SP13-439"],
             "FlexHXPlusRFCart": ["DielectricRFCart_HXPlus"],
             "FLEXJXPRFCart": ["FlexJXP_UI-SP19-527"],
-            "ExelanTopPlate": ["100A_Universal_Top_Plate", "FlexGL_Top_Plate", "Universal_Top_Plate_16CAM-HXPlus", "Universal_Top_Plate_HX_HX+_HXE-Razor", "Universal_Top_Plate_HX-HXPLUS", "FlexJXP_1.8.4-SP19-527"],
+            "ExelanTopPlate": ["100A_Universal_Top_Plate", "FlexGL_Top_Plate", "Universal_Top_Plate_16CAM-HXPlus", "Universal_Top_Plate_HX_HX+_HXE-Razor", "Universal_Top_Plate_HX-HXPLUS", "FlexJXP_1.8.4-SP19-527", "Universal_Top_Plate_HX_HX+_HXE-Razor_NoTCU", "Universal_Top_Plate_HX-HXPLUS_NoTCU"],
             "FlexFSeriesBiasElectrode": ["CW184_SP10_356_Rel_FSE_ONLY", "CW184_SP8_310_Rel"],
             "FlexFLSeriesBiasElectrode": ["FlexFLSeriesBiasElectrode"],
             "FlexGLSeriesBiasElectrode": ["FlexGLMZESC_Post_BQ"],
             "FlexGBBiasElectrode": ["FlexGB_BiasElectrode"],
             "FlexGSBiasElectrode": ["CW184_SP24_598"],
-            "FlexHXEBiasElectrode": ["CW184_SP17-510_FlexHXEBiasElectrode_NoCryo", "FlexHXEBiasElectrodeCRYO"],
+            "FlexHXEBiasElectrode": ["CW184_SP17-510_FlexHXEBiasElectrode_NoCryo", "FlexHXEBiasElectrodeCRYO", "FlexHXEBiasElectrodeCRYO_DummyESC"],
             "FlexHSeriesBiasElectrode": ["FlexHSeriesBiasElectrode_Corvus"],
             "FlexJXPBiasElectrode": ["FLEX_JXP"],
-            "FlexHXPlusBiasElectrode": ["FlexHXPlusBiasElectrode_Cryo", "FlexHXPlus - Blankoff", "FlexHXPlus", "FlexHXPlusBiasElectrodeTES20_40"],
+            "FlexHXPlusBiasElectrode": ["FlexHXPlusBiasElectrode_Cryo", "FlexHXPlus - Blankoff", "FlexHXPlus", "FlexHXPlusBiasElectrodeTES20_40", "FlexHXPlusBiasElectrode_Cryo "],
             "FlexFSeries": ["CW184_SP24_598_FLFSE_LMK", "CW184_SP24_598_FLFL_AMMP_LMK", "CW184_SP24_598_FLFX_LMK"],
             "FlexFLSeries": ["CW184_SP24_598_FlexFL_MZESC_LMK", "CW184_SP24_598_FlexFL_MZESC_AMMP_LMK", "CW184_SP24_598_FlexFL_MZESC_LMK_ESC_Blank"],
             "FlexGLSeries": ["CW184_SP24_598_FLGL_LMK"],
@@ -137,6 +140,7 @@ def sidebar_for_model_registration_form():
             "FlexGS": ["FlexGS_1.8.4_UI-SP30-B9_NoTCU"],
             "FlexHXEPM": ["CW184_SP24-598", "CW184_SP27-HF2-Razor_NoTCU", "CW184_SP30_B9-Release_NoTCU"],
             "FlexHSeries": ["184_SP24-598_X10_CPU-FlexHSeries_HX_NoTCU"],
+            "FlexHXPlus": ["CW184_SP27-HF2_HXPlusCRYO_Razor_NoTCU"],
             "FlexJXP": ["FlexJXP_1.8.4_UI-SP24-598"]
         }
         st.selectbox("ATAC Spec Model", atac_spec_model_options.get(selected_model_number, []), key='spec_model')
@@ -155,7 +159,7 @@ def sidebar_for_model_registration_form():
                 "Flex FLGB-RF", "Flex FLGS-RF",
                 "Flex HX-RF", "Flex FLHXE-RF",
                 'Flex HX+-RF',
-                "Flex HXE CRYO-RF", "Flex HXPlus CRYO-RF",
+                "Flex HXE CRYO-RF", "Flex HXPlus CRYO-RF", "Flex HXPlus_CRYO-RF",
                 "Flex JXP-RF",
             ],
             "Top plate": [
@@ -164,6 +168,7 @@ def sidebar_for_model_registration_form():
                 "Flex HX-TP", "Flex FLHXE-TP(Pinstripe)",
                 "Flex HXE CRYO-TP", "Flex HXPlus CRYO-TP",
                 "Flex HX+-TP", "Flex HX+-TP(Pinstripe)",
+                "Flex HXPlus_CRYO-TP",
                 "Flex JXP-TP",
             ],
             "Bias": [
@@ -171,14 +176,14 @@ def sidebar_for_model_registration_form():
                 "Flex FLGL-BE", "Flex FLGB-BE", "Flex FLGS-BE",
                 "Flex HXE-BE", "Flex HX-BE", "Flex FLHXE-BE", "Flex HXE-BE",
                 "Flex HXE CRYO-BE", "Flex HXPlus CRYO-BE",
-                "Flex HX-PLUS-BE",
+                "Flex HX-PLUS-BE", "Flex HXPlus_CRYO-BE",
                 "Flex JXP-BE",
             ],
             "PM": [
                 "Flex FSE-PM", "Flex FS-PM", "Flex FLFS-PM", "Flex FLFL-PM", "Flex FLFX-PM",
                 "Flex FLGL-PM", "Flex FLGB-PM", "Flex FLGS-PM",
                 "Flex HX-PM",  "Flex FLHXE-PM",
-                "Flex HXE CRYO-PM",
+                "Flex HXE CRYO-PM", "Flex HXPlus_CRYO - PM",
                 "Flex JXP-PM",
             ]
         }
@@ -211,8 +216,8 @@ def filter_lpr_model_name():
                                             "It will indicated only that.", [
         "23FLEXFSE-K", "23FLEXFS-K", "23FLEXFL-K", "23FLEXFL_AMMP-K",
         "23FLEXFX-K", "23FLEXGL-K", "23FLEXGB-K", "23FLEXGS", "23FLEXHX",
-        "23FLEXHXE", "23FLEXHXE_CRYO", "23FLEXHXPL_CRYO", "23FLEXJXP",
-        "FLEXGX/GXE-HXPL", "FLEXHXPL-HXPLC", "FLEXHXPLHXE_CM", "FLEXHX_PLUS-HXE"
+        "23FLEXHXE", "23FLEXHXE_CRYO", "23FLEXHXPL_CRYO", "23FLEXHX_PLUS", "23FLEXJXP",
+        "FLEXGX/GXE-HXPL", "FLEXHXPL-HXPLC", "FLEXHXPLHXE_CM", "FLEXHX_PLUS-HXE", "FLEXHXE-HXEC", "FLEXGX-GXE-HX_U","FLEXHX--HX_PLUS"
     ], default=[], placeholder="Select LPR Model Name to filter")
 
     # Fetch all models from the database
